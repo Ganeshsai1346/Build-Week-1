e6c9ca53073a283a3dbd1238208c0acff4cfe628
sas888 <syli888@outlook.com>
2022-05-17 16:40:45 +0100

    Update

---
+++ script.js
@@ -0,0 +1,21 @@
+var data = {
+  datasets: [
+    {
+      data: [67.7, 33.3],
+      backgroundColor: ["#00FFFF", "#D20094"],
+    },
+  ],
+};
+var promisedDeliveryChart = new Chart(
+  document.getElementById("myChart"),
+  {
+    type: "doughnut",
+    data: data,
+    options: {
+      // responsive: true,
+      cutoutPercentage: 70,
+      rotation: 120,
+
+    },
+  }
+);
