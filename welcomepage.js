5f3070dd01ccc9898a6d39b6cc14e4ba34caa688
Ganesh sai <ganeshsqai1346@gmail.com>
2022-05-18 14:23:14 +0200

    Created option for enable/disable button

--- welcomepage.js
+++ welcomepage.js
@@ -1,5 +1,13 @@
 /** @format */
 
 function enable() {
-  let checkBoxEnable = document.querySelector();
+  let checkBoxEnable = document.getElementById("checkBox");
+  let buttonProceed = document.getElementById("btn");
+  if (checkBoxEnable.checked) {
+    buttonProceed.removeAttribute("disabled");
+  } else {
+    buttonProceed.disabled = "true";
+  }
 }
+
+enable();
