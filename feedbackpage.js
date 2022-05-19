066bb99725c38d47ba93ce097923dd73dbba25d5
Ganesh sai <ganeshsqai1346@gmail.com>
2022-05-19 17:08:35 +0200

    Updated the link option in JS

--- feedbackpage.js
+++ feedbackpage.js
@@ -3,10 +3,14 @@
 function enableButton() {
   let textEnable = document.getElementById("writeComment").value;
   let buttonMoreInfo = document.getElementById("btn");
+  let linkActive = document.getElementById("link");
   if (textEnable !== "") {
     buttonMoreInfo.removeAttribute("disabled");
+    linkActive.removeAttribute("disabled");
+    linkActive.href = "https://epicode.com/en/?utm_source=strive.school";
   } else {
     buttonMoreInfo.disabled = "true";
+    linkActive.disabled = "true";
   }
 }
 
