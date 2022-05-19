fb06c812d6c939d0cc3eda58522e6c5c6a131fde
Samreensaba <Samren_saba@yahoo.com>
2022-05-19 16:57:56 +0100

    questions update

---
+++ questions.js
@@ -0,0 +1,16 @@
+let questons = [
+    {
+        question: 'How do you call a function named "foo"?',
+        answers:{
+            a: "foo;",
+            b: "call function foo();",
+            c: "foo();",
+            d: "function foo();"
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: 'Which of the following is a primitive data types in JavaScript?',
+        answers:{}
+    }
+]
