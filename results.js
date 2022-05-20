b1fd6538497c990f55185e563b12d0bc31d23bff
sas888 <syli888@outlook.com>
2022-05-20 15:01:01 +0100

    update

--- results.js
+++ results.js
@@ -1,20 +1,27 @@
-var data = {
+// let questions = 0;
+// let correctAn = 0;
+// let wrongAns = 33.3;
+
+let data = {
   datasets: [
     {
-      data: [67.7, 33.3],
+      data: [correctAn, wrongAns],
       backgroundColor: ["#00FFFF", "#D20094"],
     },
   ],
 };
-var promisedDeliveryChart = new Chart(
-  document.getElementById("myChart"),
-  {
-    type: "doughnut",
-    data: data,
-    options: {
-      cutoutPercentage: 70,
-      rotation: 120,
+let promisedDeliveryChart = new Chart(document.getElementById("myChart"), {
+  type: "doughnut",
+  data: data,
+  options: {
+    cutoutPercentage: 70,
+    rotation: 120,
+  },
+});
 
-    },
-  }
+let btnRateUs = document.querySelector("#rateUS");
+
+btnRateUs.addEventListener(
+  "click",
+  () => (btnRateUs.style.backgroundColor = "#00FFFF")
 );
